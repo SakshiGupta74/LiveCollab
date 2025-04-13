2d7e9d26f602ff7965fd9352d1815900f77c7922
SAKSHI GUPTA <147903306+SakshiGupta74@users.noreply.github.com>
2025-04-13 18:14:56 +0530

    second update

--- CodeCast/client/src/components/Login.js
+++ CodeCast/client/src/components/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
           return handleError("email and password are required");
         }
         try {
-          const url = `http://localhost:5000/auth/login`;
+          const url = `https://livecollab-eqcd.onrender.com/auth/login`;
           const response = await fetch(url, {
             method: "POST",
             headers: {
